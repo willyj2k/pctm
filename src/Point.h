8b0f8c236f7d90f51a3531617c64471b92a8fcd5
Rene Lee <renelee@berkeley.edu>
2019-04-30 19:07:56 -0700

    created point class

---
+++ src/Point.h
@@ -0,0 +1,16 @@
+//
+// Created by Rene Lee on 2019-04-30.
+//
+
+#ifndef PCTM_POINT_H
+#define PCTM_POINT_H
+
+
+class Point {
+public:
+  Vector3D position;
+  Vector3D normal;
+};
+
+
+#endif //PCTM_POINT_H
